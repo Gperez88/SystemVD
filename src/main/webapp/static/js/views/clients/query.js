e64c243d856d162ff28bb9a7cc6fd2873c218456
Gperez88 <perezgabriel89@gmail.com>
2016-05-17 22:01:25 -0400

    generic confirmation dialog

--- src/main/webapp/static/js/views/clients/query.js
+++ src/main/webapp/static/js/views/clients/query.js
@@ -5,13 +5,6 @@ $(document).ready(function () {
     util.activeNavMenuLiById('#nav-menu-li-clients');
     $.validate({
         lang: 'es',
-        modules : 'date'
+        modules: 'date'
     });
-
-    $('#removeClientModal').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget)
-        var recipient = button.data('whatever')
-        var modal = $(this)
-        modal.find('.modal-body input').val(recipient)
-    })
 });
